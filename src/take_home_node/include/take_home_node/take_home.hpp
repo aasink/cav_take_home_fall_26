8b0b32fdba58936ecd15733b5cddd6c784b7f9b2
aasink <asinkar02@gmail.com>
2026-02-16 23:15:52 -0500

    add to .hpp

--- src/take_home_node/include/take_home_node/take_home.hpp
+++ src/take_home_node/include/take_home_node/take_home.hpp
@@ -15,9 +15,22 @@ class TakeHome : public rclcpp::Node {
   void odometry_callback(nav_msgs::msg::Odometry::ConstSharedPtr odom_msg);
 
  private:
+  float wheelSlip_rear(float vx, float w, float vw, bool leftW);        // calc the wheel slips
+  float wheelSlip_front(float vx, float w, float vy, float angle, float vw, bool leftW);
+
+  const float WF = 1.638;   // front track width (meters)
+  const float WR = 1.523;    //  rear track width (meters)
+  const float LF = 1.7238;  // longitdinal dist from COG to front wheels (meters)
 
   // Subscribers and Publishers
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odometry_subscriber_;
   rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr metric_publisher_;
+
+  rclcpp::Subscription<raptor_dbw_msgs::msgs::WheelSpeedReport>::SharedPtr wspeed_subscriber_;
+  rclcpp::Subscription<raptor_dbw_msgs::msgs::SteeringExtededReport>::SharedPtr wangle_subscriber_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr wslip_rr_publisher_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr wslip_rl_publisher_;  
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr wslip_fr_publisher_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr wslip_fl_publisher_;
 
 };
