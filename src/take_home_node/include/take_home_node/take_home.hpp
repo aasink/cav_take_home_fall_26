0ebe396ebbcea8d6d38f8ad7bfa22d3c4e4dc076
aasink <asinkar02@gmail.com>
2026-02-18 18:56:52 -0500

    add callback and related pkg reqs for task 2 a

--- src/take_home_node/include/take_home_node/take_home.hpp
+++ src/take_home_node/include/take_home_node/take_home.hpp
@@ -3,6 +3,12 @@
 // Here we include message types which we can subscribe to or publish
 #include <std_msgs/msg/float32.hpp>
 #include <nav_msgs/msg/odometry.hpp> 
+#include <raptor_dbw_msgs/msg/wheel_speed_report.hpp>
+#include <raptor_dbw_msgs/msg/steering_extended_report.hpp>
+
+#include "message_filters/subscriber.h"
+#include "message_filters/synchronizer.h"
+#include "message_filters/sync_policies/approximate_time.h"
 
 #include <rclcpp/node.hpp>
 #include <rclcpp/node_options.hpp>
@@ -13,24 +19,36 @@ class TakeHome : public rclcpp::Node {
   TakeHome(const rclcpp::NodeOptions& options);
 
   void odometry_callback(nav_msgs::msg::Odometry::ConstSharedPtr odom_msg);
+  void wheelSlip_callback(nav_msgs::msg::Odometry::ConstSharedPtr odom_msg,
+                          raptor_dbw_msgs::msg::WheelSpeedReport::ConstSharedPtr wheel_msg,
+                          raptor_dbw_msgs::msg::SteeringExtendedReport::ConstSharedPtr steer_msg);
 
  private:
+  const float WF = 1.638f;   // front track width (meters)
+  const float WR = 1.523f;    //  rear track width (meters)
+  const float LF = 1.7238f;  // longitdinal dist from COG to front wheels (meters)
+
   float wheelSlip_rear(float vx, float w, float vw, bool leftW);        // calc the wheel slips
   float wheelSlip_front(float vx, float w, float vy, float angle, float vw, bool leftW);
-
-  const float WF = 1.638;   // front track width (meters)
-  const float WR = 1.523;    //  rear track width (meters)
-  const float LF = 1.7238;  // longitdinal dist from COG to front wheels (meters)
+  float convertSpeed(float kmph);                   // convert from kmph to m/s
+  float convertAngle(float deg);          // convert from deg to rad
 
   // Subscribers and Publishers
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odometry_subscriber_;
   rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr metric_publisher_;
 
-  rclcpp::Subscription<raptor_dbw_msgs::msgs::WheelSpeedReport>::SharedPtr wspeed_subscriber_;
-  rclcpp::Subscription<raptor_dbw_msgs::msgs::SteeringExtededReport>::SharedPtr wangle_subscriber_;
+  message_filters::Subscriber<nav_msgs::msg::Odometry> Wodometry_subscriber_;
+  message_filters::Subscriber<raptor_dbw_msgs::msg::WheelSpeedReport> wspeed_subscriber_;           //  subscriptions for data for wheel speed
+  message_filters::Subscriber<raptor_dbw_msgs::msg::SteeringExtendedReport> wangle_subscriber_;
+
   rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr wslip_rr_publisher_;
-  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr wslip_rl_publisher_;  
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr wslip_rl_publisher_;                // wheel slip publishers
   rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr wslip_fr_publisher_;
   rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr wslip_fl_publisher_;
+
+  std::shared_ptr<message_filters::Synchronizer<message_filters::sync_policies::ApproximateTime<
+   nav_msgs::msg::Odometry,
+   raptor_dbw_msgs::msg::WheelSpeedReport,                           // synchronizer for wheel slip
+   raptor_dbw_msgs::msg::SteeringExtendedReport>>> sync;
 
 };
